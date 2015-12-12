242d543eb5c675aa9f78bf2dc255146ed72779f8
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2015-12-12 20:20:36 +0530

    PHP script to get actor ID
    Script to fetch actor ID for the user specified actor

---
+++ php/actorId.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Daniel%20Radcliffe";
+$url = "http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=".$name;
+//echo $url.'</br>';
+$str = file_get_contents($url);
+$content = file_get_contents($url);
+$json = json_decode($content, true);
+$names = $json['name_popular'];
+$id = $names[0]['id'];
+echo $id;
+?>
