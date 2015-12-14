8b46fcd33baf502e70839a39a86fc673b35efdbb
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2015-12-15 02:43:06 +0530

    error suppression
    now returns 0 for an undefined actor name

--- phpPlusJs/actorId.php
+++ phpPlusJs/actorId.php
@@ -7,7 +7,11 @@ $url = "http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=".$name;
 $str = file_get_contents($url);
 $content = file_get_contents($url);
 $json = json_decode($content, true);
-$names = $json['name_popular'];
+$names = @$json['name_popular'];
+if($names!=null){
 $id = $names[0]['id'];
 echo $id;
+}else{
+echo '0';
+}
 ?>
