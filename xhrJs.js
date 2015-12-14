74649dfcb3035d0073371643bd34185c1b13d866
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2015-12-15 00:01:25 +0530

    Populate movie reviews
    Added function to populate movie reviews in the table

--- xhrJs.js
+++ xhrJs.js
@@ -12,17 +12,46 @@ var mov = ad.responseText;
 result = $(mov).find(".lister-item-image");
 var jsonObjects = [];
  	for(var i=0; i<3;i++){
- 		var id = $(result[i]).attr('data-tconst');
+ 		var movieId = $(result[i]).attr('data-tconst');
  	//	alert(id);
- 		var details =fetchDetails(id);	
+ 		var details = fetchDetails(movieId);	
+ 		var reviews = fetchReviews(movieId);
+ 		populateReviews(reviews.responseText,i);
  		var obj = jQuery.parseJSON(details.responseText);
  		jsonObjects.push(obj);
  		details.abort();
+
  	}
  	abc.abort(); 
  	ad.abort();
  	populateDetails(jsonObjects);  
+ 	
 }
+}
+
+function populateReviews(htmlResponse,i){
+	div = $(htmlResponse).find("#tn15content");
+	divs = $(div[0]).find("div");
+	var para = $(div[0]).find("p");
+	var para_len = $(para).size();
+	if(para_len > 0){
+		for(var j=0;j<para_len&&j<3;j++){
+				var	review = $(para[j]).html();
+				var h2 = $(divs[0]).find("h2");
+				var small = $(divs[0]).find("small");
+				var totalReviews = $(small[0]).html();
+				var reviewCount = totalReviews.split(":")[0];
+				var user = $($(divs[0]).find("a")[1]).html(); 
+				var location = $(small[1]).html();
+				var date = $(small[2]).html(); 
+				var title = $(h2).html();
+				var cell_data = '<b>'+title+'</b> by '+user+' '+location+' ('+reviewCount+')<br>'+review;
+				$('#rc-'+(i+1)+'-'+(j+1)).html(cell_data);
+		}		
+	}else{
+	
+	
+	}
 }
 
 function populateDetails(obj){
@@ -36,50 +65,35 @@ result = obj[i-1];
             var runtime = result.Runtime;
             var imdbRating = result.imdbRating;
             var imdbVotes = result.imdbVotes;
-                   var details= "Title: "+title+"| Year: "+year+"| Rated: "+rated+"| Genre: "+genre+"| Runtime: "+runtime+"| IMDB Rating: "+imdbRating+"| IMDB Votes: "+imdbVotes;
+                   var details= "Title: "+title+"</br>Year: "+year+"</br>Rated: "+rated+"</br>Genre: "+genre+"</br>Runtime: "+runtime+"</br>IMDB Rating: "+imdbRating+"</br>IMDB Votes: "+imdbVotes;
 				   $( "#r-"+i+"-3" ).html(details);
-
+				   if(thumb != 'N/A'){
+				   $( "#r-"+i+"-2" ).html('<img src='+thumb+'></img>');
+				   }else{
+				   $( "#r-"+i+"-2" ).html('No Image');
+				   }
+				   $( "#r-"+i+"-1" ).html(i);
           	}   
  	}
 
-function fetchDetails(id){
+function fetchDetails(movieId){
 return $.ajax({
-  url: "http://www.omdbapi.com/?i="+id,
+  url: "http://www.omdbapi.com/?i="+movieId,
   dataType:'json',
   async:false
   })
   .done(function( data ){
-  //return data;
   });
 
-/*var count = 0;
-for(var i=0; i<3;i++){
- 		var id = $(res[i]).attr('data-tconst');
-$.getJSON("http://www.omdbapi.com/?i="+id, function(result){
-            var thumb = result.Poster;
-            var title = result.Title;
-            var year = result.Year;
-            var rated = result.Rated;
-            var genre = result.Genre;
-            var runtime = result.Runtime;
-            var imdbRating = result.imdbRating;
-            var imdbVotes = result.imdbVotes;
-            count++;
-                   var tdPoster = document.getElementById("c-2-1");
-                   if(thumb!="N/A"){
-				   var posterNode = document.createElement("img");
-				   posterNode.src = thumb;
-				   tdPoster.appendChild(posterNode);
-				   }else{
-				   tdPoster.innerHTML = "No Image";
-				   }
-				   
-                   var tdDetails = document.getElementById("c-3-1");
-                   tdDetails.innerHTML = "Title: "+title+"| Year: "+year+"| Rated: "+rated+"| Genre: "+genre+"| Runtime: "+runtime+"| IMDB Rating: "+imdbRating+"| IMDB Votes: "+imdbVotes;
-
-            });
 }
-*/
+
+function fetchReviews(movieId){
+return $.ajax({
+  url: "phpPlusJs/fetchReviews.php?movieId="+movieId,
+  async:false
+  })
+  .done(function( data ){
+  });
 }
 
 function getActorId(first,last){
@@ -88,7 +102,6 @@ return $.ajax({
   async:false
   })
   .done(function( data ){
- // return data;
   });
 }
 
@@ -100,11 +113,6 @@ return $.ajax({
   async: false,
   })
   .done(function( data ) {
- // return data;
-/*  	result = $(data).find(".lister-item-image");
-  	for(var i=0; i<3;i++){
- 		alert($(result[i]).attr('data-tconst'));
- 	}   
-*/  });
+  });
 }
 
