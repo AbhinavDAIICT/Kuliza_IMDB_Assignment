93d75795eb7cd1ad3b741407cdec7c9f82baab46
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2015-12-16 00:41:25 +0530

    modified function call urls for hosting
    starting url path from phpPlusJs in ajax call to make it work on website

--- xhrJs.js
+++ xhrJs.js
@@ -5,23 +5,19 @@ $('#loader').show();
 if((first == null || first == "") || (last == null || last == "")){
 alert("Both fields are mandatory");
 }else{
+// clean the table before populating new results
 cleanTable();
 var jsonObjects = [];
 var movieIds = [];
 var countDetails = 0;
 var countMovies = 0;
 $.when( getActorId(first,last) ).then(function( actorId, textStatus, jqXHR ) {
-//  alert( jqXHR.status ); // Alerts 200
-//  alert(actorId);
 	$.when( getMovieId(actorId) ).then(function( movies, textStatus, jqXHR ) {
 		result = $(movies).find(".lister-item-image.ribbonize");
-//		console.log(result);
 	 	for(var i=0; i<3;i++){
 	 		var movieId = $(result[i]).attr('data-tconst');
-//	 		alert(movieId);
 	 		movieIds.push(movieId);
 	 		$.when( fetchDetails(movieId) ).then(function( details, textStatus, jqXHR ) {
-		 		//jsonObjects.push(details);
 		 		countDetails++;
 		 		populateDetails(details,countDetails,movieIds[countDetails-1]);
 	 		});
@@ -39,19 +35,22 @@ $.when( getActorId(first,last) ).then(function( actorId, textStatus, jqXHR ) {
 }
 }
 
+/*
+ *populateReviews function takes in htmlResponse and movie count as arguments.
+ *It then extracts data from the response to populate the movie review fields
+ *in the review table
+*/
 function populateReviews(htmlResponse,i){
 	div = $(htmlResponse).find("#tn15content");
 	divs = $(div[0]).find("div");
 	var para = $(div[0]).find("p");
 	var para_len = $(para).size();
-	//alert(para_len);
 	if(para_len > 0){
 		for(var j=0;j<para_len&&j<3;j++){
 				var	review = $(para[j]).html();
 				var h2 = $(divs[j*2]).find("h2");
 				var small = $(divs[j*2]).find("small");
 				var totalReviews = $(small[0]).html();
-				//var reviewCount = totalReviews.split(":")[0];
 				var user = $($(divs[j*2]).find("a")[1]).html(); 
 				var location = $(small[1]).html();
 				var date = $(small[2]).html(); 
@@ -70,6 +69,11 @@ function populateReviews(htmlResponse,i){
 	}
 }
 
+/*
+ *populateDetails function takes in json object, movie count and movie id as arguments.
+ *It then extracts data from the json objects to populate the movie detail fields
+ *in the table
+*/
 function populateDetails(obj,i,movie_id){
 result = obj;
             var thumb = result.Poster;
@@ -92,9 +96,12 @@ result = obj;
  	}
 
 
-
+/*
+ *fetchDetails function takes in movie id as argument.
+ *It fetches movie details as json objects from url http://www.omdbapi.com/?i=movieId
+ *Returns response containing json object for that movie id
+*/
 function fetchDetails(movieId){
-
 return $.ajax({
   url: "http://www.omdbapi.com/?i="+movieId,
   dataType:'json',
@@ -102,9 +109,13 @@ return $.ajax({
   })
   .done(function( data ){
   });
-
 }
 
+/*
+ *fetchReviews function takes in movie id as argument.
+ *It fetches movie details by calling php script "fetchReviews.php"
+ *Returns response containing the html content on the IMDB movie review page of the movie
+*/
 function fetchReviews(movieId){
 return $.ajax({
   url: "phpPlusJs/fetchReviews.php?movieId="+movieId,
@@ -114,26 +125,40 @@ return $.ajax({
   });
 }
 
+
+/*
+ *getActorId function takes in first and last name of the actor as argument.
+ *It fetches movie details by calling php script "actorId.php"
+ *Returns response containing actor id for the specified name
+*/
 function getActorId(first,last){
 return $.ajax({
-  url: "http://localhost/Kuliza_IMDB_Assignment/phpPlusJs/actorId.php?first="+first+"&last="+last,
+  url: "phpPlusJs/actorId.php?first="+first+"&last="+last,
  // async:false
   })
   .done(function( data ){
   });
 }
 
+/*
+ *getMovieId function takes in actor id as argument.
+ *It fetches top movies for that actor by calling php script "topMovies.php"
+ *Returns response containing html of the IMDB page containing the top movies of the specified actor
+*/
 function getMovieId(actorId){
-//alert("Movie"+actorId);
 var arr;
 return $.ajax({
-  url: "http://localhost/Kuliza_IMDB_Assignment/phpPlusJs/topMovies.php?actorId="+actorId,
+  url: "phpPlusJs/topMovies.php?actorId="+actorId,
  // async: false,
   })
   .done(function( data ) {
   });
 }
 
+
+/*
+ *cleanTable function clears table content
+*/
 function cleanTable(){
 	//clear srno, details and poster
 	for(var i=1;i<4;i++){
